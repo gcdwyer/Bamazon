724aec3e47511ef4a89f3a5bbfe7d5f9cd3ad12c
gcdwyer <gcdwyer@gmail.com>
2017-10-13 22:41:01 -0400

    display products complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,4 +15,29 @@ connection.connect(function(err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
   console.log("db connected");
+  readProducts();
 });
+
+function readProducts() {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+
+    for (var i = 0; i < res.length; i++) {
+
+    	console.log(res[i].id + " | Product: " + res[i].product_name + " | Price: $" + res[i].price);
+
+    }
+
+    buyProduct();
+
+    connection.end();
+  });
+}
+
+
+function buyProduct() {
+
+	console.log("buy product");
+
+}
